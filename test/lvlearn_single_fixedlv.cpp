8dd5f6c13d9fa3d7370947ded57908daa37d06df
degoldschmidt <goldschmidt@physik3.gwdg.de>
2015-11-21 19:44:56 +0100

    New tests with fixed landmark environment

--- test/lvlearn_single_fixedlv.cpp
+++ test/lvlearn_single_fixedlv.cpp
@@ -14,11 +14,11 @@
 using namespace std;
 
 Simulation* sim;
-const int numagents= 1;
-const int numtrials= 5;
-const double T= 500.;
-const double Thome= 300.;
-const double dt= 0.1;
+const int numagents  = 1;    /// number of agents
+const int numtrials  = 100;  /// number of trials
+const double T       = 900.; /// maximum time of a trial (500.)
+const double Thome   = 600.; /// foraging time until agent (300.)
+const double dt      = 0.1;  /// integration step size (Forward Euler method)
 
 int main(){
 	Timer timer(true);
@@ -29,12 +29,10 @@ int main(){
 	sim->add_landmark(-1., 3.);
 	sim->add_landmark(0., 0.);
 
-
 	sim->homing(false);
 	sim->gvlearn(true);
 	sim->lvlearn(true);
 	sim->init_controller(18, 1, 3, 0.05, 0.0, 0.00);
-
 
 	sim->set_inward(int(Thome/dt));
 	/*sim->c(0)->set_expl(0, 0.001, true);
